34c3cbc841d2a7dd6580234f52088905de7223f9
Ana Carolina <carolinajustinianoguzman@gmail.com>
2019-01-07 16:54:46 +0100

    mensajeria.php

---
+++ Mensajeria_php/config.php
@@ -0,0 +1,7 @@
+<?php
+    $host_db = "localhost";
+    $user_db = "root";
+    $pass_db = "";
+    $db_name = "mensajeria_php";
+    $tbl_name = "usuario";
+?>
---
+++ login/conexion.php
@@ -0,0 +1,17 @@
+<!-- 
+Creado por Luisetfree & tecnosetfree
+Web: http://luisetfree.over-blog.es
+Facebook:https://www.facebook.com/tecnosetfree/
+Twitter: @tecnosetfree
+Apoyanos con tus visitas y comentarios en nuestras redes sociales para seguir avanzando y traer contenido de calidad.
+
+ -->
+<!-- ingresa todos los datos referentes a tu conexion de base de datos
+de esta forma solo tendras que modificar un solo archivo -->
+<?php
+$host_db = "localhost";
+ $user_db = "root";
+ $pass_db = "";
+ $db_name = "basedatosmaster";
+ $tbl_name = "usuarios";
+?>
